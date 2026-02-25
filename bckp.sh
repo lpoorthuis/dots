1a9d7bc3a99d3c7ddcaf728607007eb8f99a55d1
Lukas Poorthuis <lukas.poorthuis@posteo.de>
2026-02-25 18:58:06 +0100

    update

--- bckp.sh
+++ bckp.sh
@@ -3,9 +3,9 @@
 cp $HOME/.zshrc ./
 cp -r $HOME/.dir_colors ./
 
-cp -r $HOME/.config/kitty ./.config/
+cp -r $HOME/.config/ghostty ./.config/
 cp -r $HOME/.config/nvim ./.config/
 
 if [[ "$(uname)" == "Linux" ]]; then
-    cp -r ~/.config/k*rc ./.config/
+    cp -r $HOME/.config/rofi ./.config/
 fi
--- pply.sh
+++ pply.sh
@@ -1,11 +1,10 @@
 #!/bin/zsh
-
 if [[ "$(uname)" == "Linux" ]]; then
-    cp -r .config $HOME/
-elif [[ "$(uname)" == "Darwin" ]]; then
-    cp -r .config/kitty $HOME/kitty/
-    cp -r .config/nvim $HOME/nvim/
+    cp -r .config/rofi $HOME/.config
 fi
+
+cp -r .config/ghostty $HOME/kitty/.config
+cp -r .config/nvim $HOME/nvim/.config
 
 cp -r .dir_colors $HOME/
 cp .zshrc $HOME/
